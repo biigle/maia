9d81e18e4a85f35e6ed36e5dced43bede276a50a
Martin Zurowietz <martin@zurowietz.de>
2018-12-05 10:45:59 +0100

    Improve InferenceRunner performance
    Loading of the images cost much more time than processing of the
    results, so this is done in worker threads instead now.

--- src/resources/scripts/instance-segmentation/InferenceRunner.py
+++ src/resources/scripts/instance-segmentation/InferenceRunner.py
@@ -4,7 +4,7 @@ import json
 from Config import InferenceConfig
 from Dataset import InferenceDataset
 import mrcnn.model as modellib
-from concurrent.futures import ThreadPoolExecutor, wait
+from concurrent.futures import ThreadPoolExecutor, wait, FIRST_COMPLETED
 
 class InferenceRunner(object):
 
@@ -17,34 +17,63 @@ class InferenceRunner(object):
         self.model_dir = params['model_dir']
         # Path to the trained weights to use for inference.
         self.model_path = params['model_path']
+        # We need at least one worker thread here.
+        self.max_workers = max(params['max_workers'], 1)
 
-        self.max_workers = params['max_workers']
         self.config = InferenceConfig(params, trainset)
-
         # Cast image ID to int.
         images = {int(k): v for k, v in params['images'].items()}
         self.dataset = InferenceDataset(images)
 
     def run(self):
-        # self.config.display()
+        self.config.display()
         self.dataset.prepare()
 
         model = modellib.MaskRCNN(mode="inference", config=self.config, model_dir=self.model_dir)
         model.load_weights(self.model_path, by_name=True)
 
         executor = ThreadPoolExecutor(max_workers=self.max_workers)
-        jobs = []
 
-        image_infos = self.dataset.image_info
-        total_images = len(image_infos)
+        """
+        The following is a queue-like implementation where a number of worker threads
+        load the images so the GPU is constantly fed with data. New jobs are submitted
+        only when another job finished so there is only a fixed nuber of loaded images
+        held in memory and waiting to be processed.
+        """
 
-        for i, info in enumerate(image_infos):
-            print('Image {} of {} (#{})'.format(i + 1, total_images, info['id']))
-            image = self.dataset.load_image(i)
+        # Current image index of images that were pushed to the "queue".
+        pushed_index = 0
+        # Current image index of images that have been popped from the "queue" and
+        # that have been processed.
+        popped_index = 0
+        total_images = len(self.dataset.image_info)
+
+        jobs = set()
+
+        # Initially kick off as many threads as were configured.
+        for i in range(min(self.max_workers, total_images)):
+            jobs.add(executor.submit(self.load_image, pushed_index))
+            pushed_index += 1
+
+        while popped_index < total_images:
+            # Wait for the first job that is finished.
+            done, _ = wait(jobs, return_when=FIRST_COMPLETED)
+            popped_index += 1
+            job = done.pop()
+            # Remove the job from the list so it is not returned by wait() again.
+            jobs.remove(job)
+            image, info =  job.result()
+            print('Image {} of {} (#{})'.format(popped_index, total_images, info['id']))
             results = model.detect([image])
-            jobs.append(executor.submit(self.process_result, info['id'], results[0]))
+            self.process_result(info['id'], results[0])
 
-        wait(jobs)
+            # If there are still images left, push a new job to the "queue".
+            if pushed_index < total_images:
+                jobs.add(executor.submit(self.load_image, pushed_index))
+                pushed_index += 1
+
+    def load_image(self, i):
+        return self.dataset.load_image(i), self.dataset.image_info[i]
 
     def process_result(self, image_id, result):
         points = []
@@ -58,7 +87,8 @@ class InferenceRunner(object):
             r = max(rx, ry)
             points.append([int(x + rx), int(y + ry), int(r), float(score)])
 
-        with open(os.path.join(self.tmp_dir, '{}.json'.format(image_id)), 'w') as outfile:
+        path = os.path.join(self.tmp_dir, '{}.json'.format(image_id))
+        with open(path, 'w') as outfile:
             json.dump(points, outfile)
 
 with open(sys.argv[1]) as f:
