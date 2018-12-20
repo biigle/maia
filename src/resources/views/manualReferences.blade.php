1d6a0d85fb679c31e9e1f60988c5d2790f76b46f
Martin Zurowietz <martin@zurowietz.de>
2018-12-20 13:34:04 +0100

    Add MAIA paper to the references section

---
+++ src/resources/views/manualReferences.blade.php
@@ -0,0 +1,4 @@
+<p>
+    <strong>MAIA</strong><br>
+    <a href="https://doi.org/10.1371/journal.pone.0207498">Zurowietz, M., Langenkämper, D., Hosking, B., Ruhl, H. A., & Nattkemper, T. W. (2018). MAIA—A machine learning assisted image annotation method for environmental monitoring and exploration.</a><br>PloS one, 13(11), e0207498. doi: <code>10.1371/journal.pone.0207498</code>
+</p>
