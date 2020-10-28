Fix inference on images with EXIF rotation

src/resources/scripts/instance-segmentation/Dataset.py
@@ -1,8 +1,9 @@
 import mrcnn.utils
-from skimage.io import imread
 import cv2
 import numpy as np
 import os.path
+from pyvips import Image as VipsImage
+import skimage.color
 
 class Dataset(mrcnn.utils.Dataset):
     def __init__(self, images, name='no_name', masks=[], classes={}):
@@ -22,6 +23,21 @@ class Dataset(mrcnn.utils.Dataset):
             self.add_image(self.name, image_id, image_file)
 
         super().prepare()
+
+    def load_image(self, image_id):
+        """Load the specified image and return a [H,W,3] Numpy array.
+        """
+        # Load image with Vips because if ignores EXIF rotation (as it should).
+        image = self.vips_image_to_numpy_array(VipsImage.new_from_file(self.images[imageId]))
+        # image = skimage.io.imread(self.image_info[image_id]['path'])
+        # If grayscale. Convert to RGB for consistency.
+        if image.ndim != 3:
+            image = skimage.color.gray2rgb(image)
+        # If has an alpha channel, remove it for consistency
+        if image.shape[-1] == 4:
+            image = image[..., :3]
+
+        return image
 
     def load_mask(self, image_index):
         file = self.masks[image_index]
@@ -43,6 +59,25 @@ class Dataset(mrcnn.utils.Dataset):
         masks = np.stack(masks, axis = 2).astype(np.bool)
 
         return masks, classes
+
+    def vips_image_to_numpy_array(self, image):
+        # https://libvips.github.io/pyvips/intro.html#numpy-and-pil
+        format_to_dtype = {
+            'uchar': np.uint8,
+            'char': np.int8,
+            'ushort': np.uint16,
+            'short': np.int16,
+            'uint': np.uint32,
+            'int': np.int32,
+            'float': np.float32,
+            'double': np.float64,
+            'complex': np.complex64,
+            'dpcomplex': np.complex128,
+        }
+
+        return np.ndarray(buffer=image.write_to_memory(),
+                   dtype=format_to_dtype[image.format],
+                   shape=[image.height, image.width, image.bands])
 
 class TrainingDataset(Dataset):
     def __init__(self, trainset):