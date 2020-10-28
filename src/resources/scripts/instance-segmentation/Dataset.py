03741563c6bc7bbd76e7ca968466951b926a37ad
Martin Zurowietz <martin@zurowietz.de>
2020-10-28 13:40:56 +0100

    Fix load_image function

--- src/resources/scripts/instance-segmentation/Dataset.py
+++ src/resources/scripts/instance-segmentation/Dataset.py
@@ -28,7 +28,7 @@ class Dataset(mrcnn.utils.Dataset):
         """Load the specified image and return a [H,W,3] Numpy array.
         """
         # Load image with Vips because if ignores EXIF rotation (as it should).
-        image = self.vips_image_to_numpy_array(VipsImage.new_from_file(self.images[imageId]))
+        image = self.vips_image_to_numpy_array(VipsImage.new_from_file(self.image_info[image_id]['path']))
         # image = skimage.io.imread(self.image_info[image_id]['path'])
         # If grayscale. Convert to RGB for consistency.
         if image.ndim != 3:
