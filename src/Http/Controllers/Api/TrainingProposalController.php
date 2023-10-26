d40e419ca0cb583c08f638b13530b68b3e550022
Martin Zurowietz <martin@zurowietz.de>
2023-10-26 16:57:47 +0200

    Optimize queries for similarity sort

--- src/Http/Controllers/Api/TrainingProposalController.php
+++ src/Http/Controllers/Api/TrainingProposalController.php
@@ -80,8 +80,13 @@ class TrainingProposalController extends Controller
         $feature = TrainingProposalFeatureVector::where('job_id', $id)
             ->findOrFail($id2);
 
-        $ids = $feature->nearestNeighbors('vector', Distance::Cosine)
+        // Manually optimized query for the cosine distance. The nearestNeighbors()
+        // method of pgvector seems to compute the distances twice and returns lots
+        // of data that we don't need.
+        $ids = $feature->whereNotNull('vector')
+            ->where('id', '!=', $feature->id)
             ->where('job_id', $id)
+            ->orderByRaw('vector <=> ?', [$feature->vector])
             ->pluck('id');
 
         $count = $ids->count();
