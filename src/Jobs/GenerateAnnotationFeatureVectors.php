adf6aa33a183a6ff315c34a5103d3689bf35188e
Martin Zurowietz <martin@zurowietz.de>
2023-12-20 13:10:53 +0100

    Reuse GenerateFeatureVectors job

--- src/Jobs/GenerateAnnotationFeatureVectors.php
+++ src/Jobs/GenerateAnnotationFeatureVectors.php
@@ -4,20 +4,17 @@ namespace Biigle\Modules\Maia\Jobs;
 
 use Biigle\Image;
 use Biigle\Jobs\Job;
-use Biigle\Modules\Largo\Traits\ComputesAnnotationBox;
+use Biigle\Modules\Largo\Jobs\GenerateFeatureVectors;
 use Biigle\Modules\Maia\MaiaAnnotation;
 use Biigle\Modules\Maia\MaiaJob;
 use Biigle\Shape;
 use FileCache;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Process;
-use SplFileObject;
 
-abstract class GenerateAnnotationFeatureVectors extends Job implements ShouldQueue
+abstract class GenerateAnnotationFeatureVectors extends GenerateFeatureVectors
 {
-    use SerializesModels, ComputesAnnotationBox;
+    use SerializesModels;
 
     /**
      * Number of feature vector models to insert in one chunk.
@@ -25,15 +22,6 @@ abstract class GenerateAnnotationFeatureVectors extends Job implements ShouldQue
      * @var int
      */
     const INSERT_CHUNK_SIZE = 1000;
-
-    /**
-     * The "radius" of the bounding box around a point annotation.
-     *
-     * This is half the patch size of 224 that is expected by DINO.
-     *
-     * @var int
-     */
-    const POINT_PADDING = 112;
 
     /**
      * Ignore this job if the MAIA job does not exist any more.
@@ -61,7 +49,7 @@ abstract class GenerateAnnotationFeatureVectors extends Job implements ShouldQue
         $annotations = $this->getAnnotations();
         $imageIds = $annotations->pluck('image_id')->unique();
         $images = Image::whereIn('id', $imageIds)
-            ->with('volume')
+            ->with('volume') // Required to efficiently determine the full image URL.
             ->get()
             ->all();
 
@@ -107,75 +95,4 @@ abstract class GenerateAnnotationFeatureVectors extends Job implements ShouldQue
      * Insert a chunk of new feature vector models.
      */
     abstract protected function insertFeatureVectorModelChunk(array $chunk): void;
-
-    /**
-     * Generate the input for the python script.
-     *
-     * @param \Illuminate\Support\Collection $annotations
-     */
-    protected function generateInput(array $images, array $paths, $annotations): array
-    {
-        $annotations = $annotations->groupBy('image_id');
-        $input = [];
-
-        foreach ($images as $index => $image) {
-            $path = $paths[$index];
-            $imageAnnotations = $annotations[$image->id];
-            $boxes = [];
-            foreach ($imageAnnotations as $a) {
-                $box = $this->getAnnotationBoundingBox($a->points, $a->shape, self::POINT_PADDING);
-                $box = $this->makeBoxContained($box, $image->width, $image->height);
-                $zeroSize = $box[2] === 0 && $box[3] === 0;
-
-                if (!$zeroSize) {
-                    // Convert width and height to "right" and "bottom" coordinates.
-                    $box[2] = $box[0] + $box[2];
-                    $box[3] = $box[1] + $box[3];
-
-                    $boxes[$a->id] = $box;
-                }
-            }
-
-            if (!empty($boxes)) {
-                $input[$path] = $boxes;
-            }
-        }
-
-        return $input;
-    }
-
-    /**
-     * Run the Python command.
-     *
-     * @param string $command
-     */
-    protected function python(array $input, string $outputPath)
-    {
-        $python = config('maia.python');
-        $script = config('largo.extract_features_script');
-        $inputPath = tempnam(sys_get_temp_dir(), 'maia_feature_vector_input');
-        File::put($inputPath, json_encode($input));
-        try {
-            $result = Process::forever()
-                ->env(['TORCH_HOME' => config('largo.torch_hub_path')])
-                ->run("{$python} -u {$script} {$inputPath} {$outputPath}")
-                ->throw();
-        } finally {
-            File::delete($inputPath);
-        }
-    }
-
-    /**
-     * Generator to read the output CSV row by row.
-     */
-    protected function readOuputCsv(string $path): \Generator
-    {
-        $file = new SplFileObject($path);
-        while (!$file->eof()) {
-            $csv = $file->fgetcsv();
-            if (count($csv) === 2) {
-                yield $csv;
-            }
-        }
-    }
 }
