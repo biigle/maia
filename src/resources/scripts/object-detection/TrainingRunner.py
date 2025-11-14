243261ca0384bff4a6ab5361f6716ede9458fb58
Martin Zurowietz <martin@zurowietz.de>
2025-11-14 12:44:42 +0100

    Implement TrainingRunner based on plain torchvision

--- src/resources/scripts/object-detection/TrainingRunner.py
+++ src/resources/scripts/object-detection/TrainingRunner.py
@@ -1,98 +1,126 @@
+from albumentations.pytorch import ToTensorV2
+from torch.utils.data import DataLoader
+from torch_utils import collate_fn, train_one_epoch
+from torchvision.datasets import CocoDetection
+from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+import albumentations as A
+import json
+import numpy as np
 import os
 import sys
-import json
+import torch
 
-import time
+# Based on: https://docs.pytorch.org/tutorials/intermediate/torchvision_tutorial.html#torchvision-object-detection-finetuning-tutorial
 
-from mmengine.config import Config
-from mmengine.runner import Runner
-from mmdet.utils import setup_cache_size_limit_of_dynamo
+with open(sys.argv[1]) as f:
+    params = json.load(f)
 
-class TrainingRunner(object):
+with open(sys.argv[2]) as f:
+    trainset = json.load(f)
 
-    def __init__(self, params, trainset):
-        # Path to the directory to store temporary files.
-        self.tmp_dir = params['tmp_dir']
+model = fasterrcnn_resnet50_fpn_v2(weights='DEFAULT')
+# Replace the classifier with a new one having the user-defined number of classes.
+num_classes = len(trainset['classes']) + 1  # +1 for background
+in_features = model.roi_heads.box_predictor.cls_score.in_features
+model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
-        self.base_config = params['base_config']
+transforms = A.Compose([
+    A.AtLeastOneBBoxRandomCrop(512, 512),
+    A.SomeOf([
+        A.HorizontalFlip(p=0.25),
+        A.VerticalFlip(p=0.25),
+        A.RandomRotate90(p=0.25),
+        A.GaussianBlur(sigma_limit=[1.0, 2.0]),
+        A.ImageCompression(quality_range=[25, 50]),
+    ], n=4, p=0.9),
+    A.ToFloat(),
+    ToTensorV2(),
+], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['labels']))
 
-        self.dump_config_name = 'mmdet_config.py'
-
-        self.cfg_options = {
-            # Path to store the logfiles and final checkpoint to.
-            'work_dir': os.path.join(self.tmp_dir, 'work_dir'),
-            'model': {
-                'backbone': {
-                    'init_cfg': {
-                        # Path to the pretrained weights for the backbone
-                        'checkpoint': params['backbone_model_path'],
-                    },
-                },
-            },
-            # Path to the pretrained weights for the rest of the network
-            'load_from': params['model_path'],
-            'train_dataloader': {
-                # If multi-GPU training is implemented at some point, divide this by the
-                # number of GPUs!
-                'batch_size': int(params['batch_size']),
-                'num_workers': int(params['max_workers']),
-                'dataset': {
-                    'ann_file': trainset['ann_file'],
-                    'data_prefix': {
-                        'img': trainset['img_prefix'],
-                    },
-                },
-            },
-            'test_dataloader': {
-                'dataset': {
-                    'ann_file': trainset['ann_file'],
-                    'data_prefix': {
-                        'img': trainset['img_prefix'],
-                    },
-                },
-            },
-            'test_evaluator': {
-                'ann_file': trainset['ann_file'],
-            },
-            'classes': trainset['classes'],
-            'gpu_ids': [0],
-        }
-
-    # Based on: https://github.com/open-mmlab/mmdetection/blob/master/tools/train.py
-    def run(self):
-        # Reduce the number of repeated compilations and improve
-        # training speed.
-        setup_cache_size_limit_of_dynamo()
-
-        # load config
-        cfg = Config.fromfile(self.base_config)
-
-        cfg.merge_from_dict(self.cfg_options)
-
-        if not os.path.exists(cfg.work_dir):
-            os.makedirs(cfg.work_dir)
-
-        # dump config
-        cfg.dump(os.path.join(cfg.work_dir, self.dump_config_name))
-
-        runner = Runner.from_cfg(cfg)
-        runner.train()
+class MyCocoDetection(CocoDetection):
+    def _load_target(self, id):
+        anns = self.coco.loadAnns(self.coco.getAnnIds(id))
+        img = self.coco.imgs[id]
+        boxes = [a['bbox'] for a in anns]
+        boxes = [[b[0], b[1], b[0] + b[2], b[1] + b[3]] for b in boxes]
+        labels = [a['category_id'] for a in anns]
 
         return {
-            'work_dir': cfg.work_dir,
-            'checkpoint_path': os.path.join(cfg.work_dir, 'epoch_12.pth'),
-            'config_path': os.path.join(cfg.work_dir, self.dump_config_name),
+            'boxes': boxes,
+            'labels': labels,
         }
 
-if __name__ == '__main__':
-    with open(sys.argv[1]) as f:
-        params = json.load(f)
+    def __getitem__(self, index):
+        if not isinstance(index, int):
+            raise ValueError(f"Index must be of type integer, got {type(index)} instead.")
 
-    with open(sys.argv[2]) as f:
-        trainset = json.load(f)
+        id = self.ids[index]
+        image = self._load_image(id)
+        target = self._load_target(id)
 
-    runner = TrainingRunner(params, trainset)
-    output = runner.run()
+        if self.transforms is not None:
+            ret = self.transforms(
+                image=np.array(image),
+                bboxes=np.array(target['boxes'], dtype=np.float32),
+                labels=target['labels']
+            )
+            image = ret['image']
+            target['boxes'] = torch.tensor(ret['bboxes'])
+            target['labels'] = torch.tensor(ret['labels'], dtype=torch.int64)
 
-    with open(params['output_path'], 'w') as f:
-        json.dump(output, f)
+        return image, target
+
+dataset = MyCocoDetection(root=trainset['img_prefix'], annFile=trainset['ann_file'], transforms=transforms)
+
+data_loader = DataLoader(
+    dataset,
+    batch_size=int(params['batch_size']),
+    shuffle=True,
+    num_workers=int(params['max_workers']),
+    collate_fn=collate_fn
+)
+
+# Freeze the backbone.
+for param in model.backbone.parameters():
+    param.requires_grad = False
+
+optim_params = [p for p in model.parameters() if p.requires_grad]
+optimizer = torch.optim.SGD(
+    optim_params,
+    lr=0.005,
+    momentum=0.9,
+    weight_decay=0.0005
+)
+
+lr_scheduler = torch.optim.lr_scheduler.StepLR(
+    optimizer,
+    step_size=3,
+    gamma=0.1
+)
+
+device = torch.accelerator.current_accelerator() if torch.accelerator.is_available() else torch.device('cpu')
+model.to(device)
+
+# TODO
+# num_epochs = 12
+num_epochs = 2
+
+for epoch in range(num_epochs):
+    # train for one epoch, printing every 10 iterations
+    train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
+    # update the learning rate
+    lr_scheduler.step()
+
+checkpoint_path = os.path.join(params['tmp_dir'], 'model.pth')
+torch.save(model.state_dict(), checkpoint_path)
+
+# TODO
+output = {
+    # 'work_dir': cfg.work_dir,
+    'checkpoint_path': checkpoint_path,
+    # 'config_path': os.path.join(cfg.work_dir, self.dump_config_name),
+}
+
+with open(params['output_path'], 'w') as f:
+    json.dump(output, f)
---
+++ src/resources/scripts/object-detection/torch_utils.py
@@ -0,0 +1,270 @@
+import datetime
+import time
+from collections import defaultdict, deque
+import math
+
+import torch
+import torch.distributed as dist
+
+# Based on https://github.com/pytorch/vision/blob/main/references/detection/utils.py
+# and https://github.com/pytorch/vision/blob/main/references/detection/engine.py
+
+"""
+BSD 3-Clause License
+
+Copyright (c) Soumith Chintala 2016,
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+def collate_fn(batch):
+    return tuple(zip(*batch))
+
+
+def get_world_size():
+    if not is_dist_avail_and_initialized():
+        return 1
+    return dist.get_world_size()
+
+def is_dist_avail_and_initialized():
+    if not dist.is_available():
+        return False
+    if not dist.is_initialized():
+        return False
+    return True
+
+def reduce_dict(input_dict, average=True):
+    """
+    Args:
+        input_dict (dict): all the values will be reduced
+        average (bool): whether to do average or sum
+    Reduce the values in the dictionary from all processes so that all processes
+    have the averaged results. Returns a dict with the same fields as
+    input_dict, after reduction.
+    """
+    world_size = get_world_size()
+    if world_size < 2:
+        return input_dict
+    with torch.inference_mode():
+        names = []
+        values = []
+        # sort the keys so that they are consistent across processes
+        for k in sorted(input_dict.keys()):
+            names.append(k)
+            values.append(input_dict[k])
+        values = torch.stack(values, dim=0)
+        dist.all_reduce(values)
+        if average:
+            values /= world_size
+        reduced_dict = {k: v for k, v in zip(names, values)}
+    return reduced_dict
+
+
+class SmoothedValue:
+    """Track a series of values and provide access to smoothed values over a
+    window or the global series average.
+    """
+
+    def __init__(self, window_size=20, fmt=None):
+        if fmt is None:
+            fmt = "{median:.4f} ({global_avg:.4f})"
+        self.deque = deque(maxlen=window_size)
+        self.total = 0.0
+        self.count = 0
+        self.fmt = fmt
+
+    def update(self, value, n=1):
+        self.deque.append(value)
+        self.count += n
+        self.total += value * n
+
+    def synchronize_between_processes(self):
+        """
+        Warning: does not synchronize the deque!
+        """
+        if not is_dist_avail_and_initialized():
+            return
+        t = torch.tensor([self.count, self.total], dtype=torch.float64, device="cuda")
+        dist.barrier()
+        dist.all_reduce(t)
+        t = t.tolist()
+        self.count = int(t[0])
+        self.total = t[1]
+
+    @property
+    def median(self):
+        d = torch.tensor(list(self.deque))
+        return d.median().item()
+
+    @property
+    def avg(self):
+        d = torch.tensor(list(self.deque), dtype=torch.float32)
+        return d.mean().item()
+
+    @property
+    def global_avg(self):
+        return self.total / self.count
+
+    @property
+    def max(self):
+        return max(self.deque)
+
+    @property
+    def value(self):
+        return self.deque[-1]
+
+    def __str__(self):
+        return self.fmt.format(
+            median=self.median, avg=self.avg, global_avg=self.global_avg, max=self.max, value=self.value
+        )
+
+class MetricLogger:
+    def __init__(self, delimiter="\t"):
+        self.meters = defaultdict(SmoothedValue)
+        self.delimiter = delimiter
+
+    def update(self, **kwargs):
+        for k, v in kwargs.items():
+            if isinstance(v, torch.Tensor):
+                v = v.item()
+            assert isinstance(v, (float, int))
+            self.meters[k].update(v)
+
+    def __getattr__(self, attr):
+        if attr in self.meters:
+            return self.meters[attr]
+        if attr in self.__dict__:
+            return self.__dict__[attr]
+        raise AttributeError(f"'{type(self).__name__}' object has no attribute '{attr}'")
+
+    def __str__(self):
+        loss_str = []
+        for name, meter in self.meters.items():
+            loss_str.append(f"{name}: {str(meter)}")
+        return self.delimiter.join(loss_str)
+
+    def synchronize_between_processes(self):
+        for meter in self.meters.values():
+            meter.synchronize_between_processes()
+
+    def add_meter(self, name, meter):
+        self.meters[name] = meter
+
+    def log_every(self, iterable, print_freq, header=None):
+        i = 0
+        if not header:
+            header = ""
+        start_time = time.time()
+        end = time.time()
+        iter_time = SmoothedValue(fmt="{avg:.4f}")
+        data_time = SmoothedValue(fmt="{avg:.4f}")
+        space_fmt = ":" + str(len(str(len(iterable)))) + "d"
+        if torch.cuda.is_available():
+            log_msg = self.delimiter.join(
+                [
+                    header,
+                    "[{0" + space_fmt + "}/{1}]",
+                    "eta: {eta}",
+                    "{meters}",
+                    "time: {time}",
+                    "data: {data}",
+                    "max mem: {memory:.0f}",
+                ]
+            )
+        else:
+            log_msg = self.delimiter.join(
+                [header, "[{0" + space_fmt + "}/{1}]", "eta: {eta}", "{meters}", "time: {time}", "data: {data}"]
+            )
+        MB = 1024.0 * 1024.0
+        for obj in iterable:
+            data_time.update(time.time() - end)
+            yield obj
+            iter_time.update(time.time() - end)
+            if i % print_freq == 0 or i == len(iterable) - 1:
+                eta_seconds = iter_time.global_avg * (len(iterable) - i)
+                eta_string = str(datetime.timedelta(seconds=int(eta_seconds)))
+                if torch.cuda.is_available():
+                    print(
+                        log_msg.format(
+                            i,
+                            len(iterable),
+                            eta=eta_string,
+                            meters=str(self),
+                            time=str(iter_time),
+                            data=str(data_time),
+                            memory=torch.cuda.max_memory_allocated() / MB,
+                        )
+                    )
+                else:
+                    print(
+                        log_msg.format(
+                            i, len(iterable), eta=eta_string, meters=str(self), time=str(iter_time), data=str(data_time)
+                        )
+                    )
+            i += 1
+            end = time.time()
+        total_time = time.time() - start_time
+        total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+        print(f"{header} Total time: {total_time_str} ({total_time / len(iterable):.4f} s / it)")
+
+def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, scaler=None):
+    model.train()
+    metric_logger = MetricLogger(delimiter="  ")
+    metric_logger.add_meter("lr", SmoothedValue(window_size=1, fmt="{value:.6f}"))
+    header = f"Epoch: [{epoch}]"
+
+    for images, targets in metric_logger.log_every(data_loader, print_freq, header):
+        images = list(image.to(device) for image in images)
+        targets = [{k: v.to(device) if isinstance(v, torch.Tensor) else v for k, v in t.items()} for t in targets]
+        with torch.cuda.amp.autocast(enabled=scaler is not None):
+            loss_dict = model(images, targets)
+            losses = sum(loss for loss in loss_dict.values())
+
+        # reduce losses over all GPUs for logging purposes
+        loss_dict_reduced = reduce_dict(loss_dict)
+        losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+
+        loss_value = losses_reduced.item()
+
+        if not math.isfinite(loss_value):
+            print(f"Loss is {loss_value}, stopping training")
+            print(loss_dict_reduced)
+            sys.exit(1)
+
+        optimizer.zero_grad()
+        if scaler is not None:
+            scaler.scale(losses).backward()
+            scaler.step(optimizer)
+            scaler.update()
+        else:
+            losses.backward()
+            optimizer.step()
+
+        metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
+        metric_logger.update(lr=optimizer.param_groups[0]["lr"])
+
+    return metric_logger
